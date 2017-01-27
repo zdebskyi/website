a9fdf83d35ae7278dc809aa4c26563c6508916c1
zdebskyi <zdebskyi@meta.ua>
2017-01-27 11:34:00 +0200

    Applicatoin

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zdebskyi on 27.01.17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main (String [] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
