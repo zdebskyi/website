f298ab499dcc9515f83b197db2f0bedc9e59f418
zdebskyi <zdebskyi@meta.ua>
2017-01-27 11:42:59 +0200

    model

--- src/main/java/model/News.java
+++ src/main/java/model/News.java
@@ -4,4 +4,38 @@ package model;
  * Created by zdebskyi on 27.01.17.
  */
 public class News {
+
+    private int id;
+    private String path;
+    private int idPlace;
+
+    public News(int id, String path, int idPlace) {
+        this.id = id;
+        this.path = path;
+        this.idPlace = idPlace;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getIdPlace() {
+        return idPlace;
+    }
+
+    public void setIdPlace(int idPlace) {
+        this.idPlace = idPlace;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,4 +4,58 @@ package model;
  * Created by zdebskyi on 27.01.17.
  */
 public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+
+    public User(int id, String firstName, String lastName, String login, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
